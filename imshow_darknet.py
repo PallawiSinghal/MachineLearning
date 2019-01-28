071a2cef3aebba3b85eaf9f162d659491ced1be3
Pallawi <pallawi.ds@gmail.com>
2019-01-28 17:51:11 +0530

    Update imshow_darknet.py

--- imshow_darknet.py
+++ imshow_darknet.py
@@ -46,7 +46,7 @@ class METADATA(Structure):
     
 
 #lib = CDLL("/home/pjreddie/documents/darknet/libdarknet.so", RTLD_GLOBAL)
-lib = CDLL("/home/ubuntu/workspace/darknet/libdarknet.so", RTLD_GLOBAL)
+lib = CDLL("/darknet/libdarknet.so", RTLD_GLOBAL)
 lib.network_width.argtypes = [c_void_p]
 lib.network_width.restype = c_int
 lib.network_height.argtypes = [c_void_p]
